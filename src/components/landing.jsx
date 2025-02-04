b4e6e548372ff87241ca0dae6502b4fa7590f8ef
Arya Gupta <artagupta239gmail.com>
2025-02-05 01:18:46 +0530

    image edited

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -4,20 +4,11 @@ import './Landing.css'
 const Landing = () => {
     return(
         <main>
-            <div className="left">
-                <h1>The Lit Nook</h1>
-                <p>A cozy corner for literature enthusiasts.</p>
-                <button>Get started</button>
-            </div>
-            <div className="right">
-                <p>The Lit Nook is an online bookstore designed for passionate readers who crave captivating stories and literary gems.
-                    Whether you're a fan of timeless classics, thrilling mysteries, or the latest bestsellers, our curated collection ensures there's something for everyone. 
-                    At The Lit Nook, we believe that every book is a doorway to a new adventure, a fresh perspective, or a deep emotional journey.
-                    Our user-friendly platform makes it easy to browse, discover, and purchase books from the comfort of your home.
-                    With personalized recommendations, exclusive discounts, and a vibrant community of book lovers, we aim to create more than just a bookstore—we're building a literary haven. 
-                    Dive into our ever-growing library and let your next great read find you.
-                    Because at The Lit Nook, every story deserves to be told, and every reader deserves the perfect book.</p>
-            </div>
+            <h1>The Lit Nook</h1>
+            <p>your cozy corner for discovering and buying books online! Explore a vast collection of bestsellers, timeless classics, and hidden gems, all at your fingertips. 
+                    Find your next great read and let every page take you on a new adventure!
+            </p>
+            <button>Get started</button>
         </main>
     )
 }
