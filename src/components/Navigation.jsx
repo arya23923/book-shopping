185807aa039833508201826c9a456643fe0443db
Arya Gupta <artagupta239gmail.com>
2025-02-05 19:23:38 +0530

    library image modified

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from './images/the lit nook logo.png'
 
 const Navigation = () => {
     return(
@@ -8,7 +9,11 @@ const Navigation = () => {
             <a>SUBSCRIPTION</a>
             <a>ABOUT</a>
             <a>INSPIRATION </a>
-            <a></a>
+            <a>CONTACT</a>
+            <a><img src={logo} /></a>
+            <a>SEARCH</a>
+            <a>ACCOUNT</a>
+            <a>CART</a>
         </nav>
     )
 }
