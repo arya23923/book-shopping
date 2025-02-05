c37335aa03e9a09f05fd2ad42a70172d2df09502
Arya Gupta <artagupta239gmail.com>
2025-02-05 22:30:24 +0530

    navigation bar desktop layout done

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,18 +3,27 @@ import { Link } from "react-router-dom";
 import Logo from './images/the lit nook logo.png'
 import './Navigation.css'
 import inverted from './images/lit nook logo inverted.png'
+import search_icon from './images/search.png'
+import search_invert from './images/search-inverted.png'
 
 const Navigation = () => {
     const [logo, setLogo] = useState(inverted)
+    const [search, setSearch] = useState(search_invert)
     return(
         <nav
-            onMouseEnter={() => (setLogo(Logo))}
-            onMouseLeave={() => (setLogo(inverted))}>
+            // onMouseEnter={() => (setLogo(Logo))}
+            // onMouseLeave={() => (setLogo(inverted))}
+            onMouseEnter={() => {setLogo(Logo); setSearch(search_icon)}}
+            onMouseLeave={() => {setLogo(inverted); setSearch(search_invert)}}
+        >
             <a>SHOP</a>
             <a>ABOUT</a>
             <a>CONTACT</a>
             <a><img src={logo} /></a>
-            <a>SEARCH</a>
+            <span>
+                <img src={search} />
+                <a>SEARCH</a>
+            </span>
             <a>ACCOUNT</a>
             <a>CART</a>
         </nav>
