638fcfae6eded26365bbc9732909134e0e43c082
Arya Gupta <artagupta239gmail.com>
2025-02-05 19:17:35 +0530

    logo added to images folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Landing from './components/Landing'
+import Homepage from './components/Homepage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element = {<Landing />} />
+        <Route path='/' element = {<Homepage />} />
+        <Route path='/landing' element={<Landing />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navigation = () => {
+    return(
+        <nav>
+            <a>SHOP</a>
+            <a>SUBSCRIPTION</a>
+            <a>ABOUT</a>
+            <a>INSPIRATION </a>
+            <a></a>
+        </nav>
+    )
+}
+
+export default Navigation;
